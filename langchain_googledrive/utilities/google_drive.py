9e25ad53ca929f6d2722339167c4371d972b4c88
Patrick Cheng <patrickyccheng@gmail.com>
2024-09-23 12:16:34 -0700

    rollback change add added mypy plugin instead.

--- langchain_googledrive/utilities/google_drive.py
+++ langchain_googledrive/utilities/google_drive.py
@@ -496,10 +496,6 @@ class GoogleDriveUtilities(Serializable, BaseModel):
 
     not_data: UUID = uuid4()
 
-    # not used, but added to avoid call-arg errors
-    folder_id: Optional[str] = None
-    mime_type: Optional[str] = None
-
     # pre=True, always=True
     @model_validator(mode="before")
     @classmethod
